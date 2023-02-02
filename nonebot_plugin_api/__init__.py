054589e89f27128ba157722a5a9ab2982cc137f5
Agnes Digital <70925546+Umamusume-Agnes-Digital@users.noreply.github.com>
2023-02-02 10:19:05 +0800

    首次提交
    首次提交

---
+++ nonebot_plugin_api/__init__.py
@@ -0,0 +1,13 @@
+from nonebot.plugin import PluginMetadata
+
+
+__version__ = "0.0.1"
+__plugin_meta__ = PluginMetadata(
+    name="三方api管理",
+    description='建议三方api进行调用',
+    usage='三方api',
+    extra={
+        "version": __version__,
+        "author": "Umamusume-Agnes-Digital <Z735803792@163.com>",
+    },
+)
